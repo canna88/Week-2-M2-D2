5a459f649b022a28ef326a18b752bf3788c550aa
canna88 <97223496+canna88@users.noreply.github.com>
2023-06-22 07:39:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//Domanda 1
+    // Number: è un tipo di variabile che contiene un numero, che può essere float o int
+    // String: contiene una serie di caratterti che, nella fase di definizione, devono essere contenuti nelle virgolette ""
+    // Boolean: è una variabile di tipo logico, può contenere 2 valori: True o False
+    // Undefined: è una variabile il cui contenuto non è ancora stato definito
+    // Null: è una variabile che contiene il valore null, un valore vuoto
+    
+//Domanda 2
+
+let myVar = 12 + 20
+console.log("Domanda 2: ",myVar)
+
+//Domanda 3
+
+let x = 12
+console.log("Domanda 3: ",x)
+
+//Domanda 4
+
+let name = "Alessio"
+console.log("Domanda 4: ",name)
+
+//Domanda 5
+console.log("Domanda 5: ",4-x)
+
+//Domanda 6
+let studentMe = {
+    name: "Alessio",
+    surname : "Canna",
+    hobby : "coding",
+}
+
+console.log("Domanda 6: ",studentMe)
+
+//Domanda 7
+
+let name1 = "john"
+let name2 = "John"
+console.log("Domanda 7 - 1° verifica: ",name1 !== name2)
+console.log("Domanda 7 - 2° verifica: ",name1 == name2.toLowerCase(), "-> il valore della variabile name2 non è cambiato: ", name2)
+
